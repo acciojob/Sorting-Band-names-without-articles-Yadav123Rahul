f77f4f7a4c9b731f6100e672aeb64d86cacad339
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-08-18 14:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const bandNames = [
+    "The Rolling Stones",
+    "Led Zeppelin",
+    "The Beatles",
+    "Queen",
+    "Metallica",
+    "Pink Floyd",
+    "The Who",
+  ];
+
+  function stripArticle(name) {
+    return name.replace(/^(a|an|the) /i, "").trim();
+  }
+
+  const sortedBandNames = bandNames.slice().sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+
+  const ulElement = document.querySelector("ul");
+
+  for (let i = 0; i < sortedBandNames.length; i++) {
+    const liElement = document.createElement("li");
+    liElement.textContent = sortedBandNames[i];
+    ulElement.appendChild(liElement);
+  }
+});
